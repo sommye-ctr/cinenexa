731b3991c4eb44e9ebf558b9e41ac0e05b64665d
Somye <anyone.mahajan@gmail.com>
2022-01-07 11:48:46 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:watrix/resources/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: Constants.appName,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Text("This is movie streaming app - Watrix"),
+    );
+  }
+}
