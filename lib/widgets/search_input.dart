d54489002f4a22ec51884bfcd8b0b47704f63b8f
Somye <anyone.mahajan@gmail.com>
2022-06-13 16:27:11 +0530

    Added clear button in search

--- lib/widgets/search_input.dart
+++ lib/widgets/search_input.dart
@@ -4,11 +4,12 @@ import '../resources/strings.dart';
 import '../resources/style.dart';
 import '../utils/screen_size.dart';
 
-class SearchInput extends StatelessWidget {
+class SearchInput extends StatefulWidget {
   final TextEditingController? controller;
   final Function(String)? onChanged;
   final Function() onEditingComplete;
   final String? value, hint;
+
   SearchInput({
     Key? key,
     this.onChanged,
@@ -19,14 +20,57 @@ class SearchInput extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<SearchInput> createState() => _SearchInputState();
+}
+
+class _SearchInputState extends State<SearchInput> {
+  final FocusNode focusNode = FocusNode();
+
+  Widget suffixWidget = IconButton(
+    icon: Icon(Icons.search),
+    onPressed: () {},
+    color: Colors.black,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    focusNode.addListener(() {
+      if (focusNode.hasFocus) {
+        setState(() {
+          suffixWidget = IconButton(
+            onPressed: () {
+              focusNode.unfocus();
+              widget.controller?.clear();
+            },
+            icon: Icon(
+              Icons.cancel,
+              color: Colors.black,
+            ),
+          );
+        });
+        return;
+      }
+      suffixWidget = IconButton(
+        icon: Icon(
+          Icons.search_rounded,
+          color: Colors.black,
+        ),
+        onPressed: () {},
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       width: ScreenSize.getPercentOfWidth(context, 0.95),
       child: TextFormField(
-        initialValue: value,
-        controller: controller,
-        onChanged: onChanged,
-        onEditingComplete: onEditingComplete,
+        focusNode: focusNode,
+        initialValue: widget.value,
+        controller: widget.controller,
+        onChanged: widget.onChanged,
+        onEditingComplete: widget.onEditingComplete,
         style: TextStyle(
           color: Colors.black,
         ),
@@ -37,12 +81,9 @@ class SearchInput extends StatelessWidget {
             borderRadius: BorderRadius.circular(Style.largeRoundEdgeRadius),
             borderSide: BorderSide.none,
           ),
-          hintText: hint ?? Strings.search,
+          hintText: widget.hint ?? Strings.search,
           hintStyle: TextStyle(color: Colors.black),
-          suffixIcon: Icon(
-            Icons.search,
-            color: Colors.black,
-          ),
+          suffixIcon: suffixWidget,
         ),
         textInputAction: TextInputAction.search,
         textCapitalization: TextCapitalization.words,
