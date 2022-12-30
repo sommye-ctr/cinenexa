c3f1927f870ab2d3a6ccba57f2f7cf6c48562389
Somye Mahajan <anyone.mahajan@gmail.com>
2022-12-31 01:06:03 +0530

    Fixed passing installed extensions

--- lib/screens/details_page.dart
+++ lib/screens/details_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:watrix/components/details_header.dart';
 import 'package:watrix/components/details_more_details.dart';
+import 'package:watrix/models/network/extensions/extension.dart';
 
 import 'package:watrix/services/constants.dart';
 import 'package:watrix/store/details/details_store.dart';
@@ -37,12 +38,13 @@ class _DetailsPageState extends State<DetailsPage> {
 
   @override
   void initState() {
+    var list = Provider.of<ExtensionsStore>(context, listen: false)
+        .installedExtensions;
     detailsStore = DetailsStore(
-      baseModel: widget.baseModel,
-      noOfExtensions: Provider.of<ExtensionsStore>(context, listen: false)
-          .installedExtensions
-          .length,
-    );
+        baseModel: widget.baseModel,
+        noOfExtensions: list.length,
+        installedExtensions:
+            list.map((element) => element.getExtension()).toList());
     super.initState();
   }
 
