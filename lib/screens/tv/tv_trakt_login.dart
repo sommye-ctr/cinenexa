7fb88c50b4dca1717d33a3f78a71324d9da9f1f5
Somye Mahajan <anyone.mahajan@gmail.com>
2023-06-22 00:58:53 +0530

    Added trakt login page

---
+++ lib/screens/tv/tv_trakt_login.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TvTraktLogin extends StatelessWidget {
+  const TvTraktLogin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
